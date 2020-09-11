629385fbe035c23bf1b65b5b6ce89fea3170e099
Paulo <al.pdias24@gmail.com>
2020-09-10 22:42:12 -0300

    update erros html

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ Created in 09/2020
 
 let buttonElement = document.querySelector('.main-menu__btn');
 let menuElement = document.querySelector('.main-menu__list');
-let item1 = document.getElementById('1one');
-let item2 = document.getElementById('2two');
-let item3 = document.getElementById('3three');
+let item1 = document.getElementById('b1one');
+let item2 = document.getElementById('b2two');
+let item3 = document.getElementById('b3three');
 
 function openClose(evento) {
     buttonElement.classList.toggle('main-menu__btn--close');
